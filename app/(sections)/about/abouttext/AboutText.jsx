ff93e7b68275d55924a1df6723da886fc1ba527b
Jose <joseernestoroldan@gmail.com>
2024-03-21 21:16:02 -0400

    deploying error: escape characters

--- app/(sections)/about/abouttext/AboutText.jsx
+++ app/(sections)/about/abouttext/AboutText.jsx
@@ -9,14 +9,14 @@ const AboutText = () => {
         <h2 className={styles.heading}>About me</h2>
         <p className={styles.paragraph}>
           {" "}
-          Greetings! I'm Jose, a seasoned software developer with a passion for
+          Greetings! I&apos;m Jose, a seasoned software developer with a passion for
           crafting exceptional digital experiences. With a robust skill set
           spanning a variety of technologies and frameworks, I specialize in
           delivering cutting-edge web solutions that captivate and inspire.
         </p>
         <p className={styles.paragraph}>
           My journey into the world of software development began with an
-          insatiable curiosity and a drive for innovation. Over the years, I've
+          insatiable curiosity and a drive for innovation. Over the years, I&apos;ve
           honed my expertise in frontend development, mastering the intricacies
           of JavaScript and its popular libraries and frameworks like ReactJS,
           Angular, and Vue.js. From crafting sleek user interfaces to
@@ -24,11 +24,11 @@ const AboutText = () => {
           life with precision and finesse.
         </p>
         <p className={styles.paragraph}>
-          But my expertise doesn't end at the frontend. I'm well-versed in
-          backend technologies too. Whether it's leveraging the flexibility of
+          But my expertise doesn&apos;t end at the frontend. I&apos;m well-versed in
+          backend technologies too. Whether it&apos;s leveraging the flexibility of
           composable content clouds like Sanity or the efficiency of Prisma ORM,
           I specialize in building robust backend solutions that power seamless
-          user experiences. I'm proficient in Node.js for server-side
+          user experiences. I&apos;m proficient in Node.js for server-side
           development and proficient in managing data with databases such as
           MongoDB, MySQL, and PostgreSQL.
         </p>
@@ -40,15 +40,15 @@ const AboutText = () => {
           expectations.
         </p>
         <p className={styles.paragraph}>
-          When I'm not immersed in code, you can find me staying up-to-date with
+          When I&apos;m not immersed in code, you can find me staying up-to-date with
           the latest trends in technology, experimenting with new tools and
           frameworks, or indulging in outdoor adventures.
         </p>
         <p className={styles.paragraph}>
-          Whether you're a startup looking to establish your online presence, an
+          Whether you&apos;re a startup looking to establish your online presence, an
           enterprise seeking to optimize your backend operations, or an
-          individual with a bold digital vision, I'm here to help turn your
-          ideas into reality. Let's collaborate and create something remarkable
+          individual with a bold digital vision, I&apos;m here to help turn your
+          ideas into reality. Let&apos;s collaborate and create something remarkable
           together!
         </p>
       </div>
