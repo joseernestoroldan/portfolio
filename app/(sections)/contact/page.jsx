44a84316899c7df084922332c871c1fae55f606d
Jose <joseernestoroldan@gmail.com>
2024-03-25 18:10:03 -0400

    Featured: Contact Layout and Styles

--- app/(sections)/contact/page.jsx
+++ app/(sections)/contact/page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import generalStyles from "./../sectiongeneral.module.css";
+import styles from "./contact.module.css";
 
 const ContactPage = () => {
   return (
@@ -7,7 +8,28 @@ const ContactPage = () => {
       <div className={generalStyles.blur}>
         <div className={generalStyles.center}></div>
       </div>
-      <div>Contact</div>
+      <div className={styles.container}>
+        <h3>Take A Coffee With Me</h3>
+        <form className={styles.form}>
+          <label className={styles.label} htmlFor="name">Name:</label>
+          <div className={styles.fieldContainer}>
+            <input className={styles.input} name="name" type="text" />
+          </div>
+          <label className={styles.label} htmlFor="email">Email:</label>
+          <div className={styles.fieldContainer}>
+            <input className={styles.input} name="email" type="email" />
+          </div>
+          <label className={styles.label} htmlFor="telephone">Telephone:</label>
+          <div className={styles.fieldContainer}>
+            <input className={styles.input} name="telephone" type="tel" />
+          </div>
+          <label className={styles.label} htmlFor="message">Message:</label>
+
+          <textarea name="message" id="" className={styles.textArea} cols="30" rows="10"></textarea>
+
+          <button className={styles.button}>Enviar</button>
+        </form>
+      </div>
     </div>
   );
 };
