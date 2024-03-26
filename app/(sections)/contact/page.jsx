7a9733d9912cbdb080b07a0a1f6da9ff67521009
Jose <joseernestoroldan@gmail.com>
2024-03-26 16:50:42 -0400

    Implementation of contact page, conection to sanity

--- app/(sections)/contact/page.jsx
+++ app/(sections)/contact/page.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import generalStyles from "./../sectiongeneral.module.css";
 import styles from "./contact.module.css";
-import Form from "./form/form";
-
+import { client } from "@/client";
 
 const ContactPage = () => {
+
+  async function addContact(formData){
+    'use server'
+    const {name, email, number, message} = Object.fromEntries(formData)
+
+    const contact = {
+      _type: "contact",
+      name: name,
+      email: email,
+      number: number,
+      message: message,
+    };
+
+    try {
+      await client.create(contact)
+    } catch (error) {
+      console.log(error)
+      throw new Error("Failed to create user")
+    }
+  }
 
 
   return (
@@ -14,7 +33,56 @@ const ContactPage = () => {
       </div>
       <div className={styles.container}>
         <h3>Take A Coffee With Me</h3>
-        <Form/>
+        <form action={addContact} className={styles.form}>
+      <label className={styles.label} htmlFor="name">
+        Name:
+      </label>
+      <div className={styles.fieldContainer}>
+        <input
+          className={styles.input}
+          name="name"
+          type="text"
+
+        />
+      </div>
+      <label className={styles.label} htmlFor="email">
+        Email:
+      </label>
+      <div className={styles.fieldContainer}>
+        <input
+          className={styles.input}
+          name="email"
+          type="email"
+
+        />
+      </div>
+      <label className={styles.label} htmlFor="number">
+        Number:
+      </label>
+      <div className={styles.fieldContainer}>
+        <input
+          className={styles.input}
+          name="number"
+          type="tel"
+
+        />
+      </div>
+      <label className={styles.label} htmlFor="message">
+        Message:
+      </label>
+
+      <textarea
+        name="message"
+        id=""
+        className={styles.textArea}
+        cols="30"
+        rows="10"
+      ></textarea>
+
+      <button className={styles.button}>
+        Enviar
+      </button>
+    </form>
       </div>
     </div>
   );
